b19294ceec9374ee1a8a113f928936b5e6915ca2
filipfurlan <filip.furlan11@gmail.com>
2025-02-17 10:42:09 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const points = [234, 2, 234, 10, 122, 10, 122, 26, 202, 26, 202, 42, 170, 42, 17
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+var je = false;
+var interval;
 const x = [234, 234, 122, 122, 202, 202, 170, 170, 154, 154, 170, 170, 186, 186, 138, 138, 90, 90, 58, 58, 90, 90, 74, 74, 106, 106, 122, 122, 106, 106, 74, 74, 106, 106, 122, 122, 138, 138, 154, 154, 138, 138, 154, 154, 138, 138, 170, 170, 186, 186, 202, 202, 218, 218, 202, 202, 218, 218, 202, 202, 234, 234, 266, 266, 250, 250, 202, 202, 218, 218, 250, 250, 282, 282, 298, 298, 266, 266, 234, 234, 250, 250];
 const y = [2, 10, 10, 26, 26, 42, 42, 106, 106, 154, 154, 138, 138, 170, 170, 202, 202, 234, 234, 250, 250, 266, 266, 330, 330, 362, 362, 378, 378, 394, 394, 410, 410, 426, 426, 410, 410, 426, 426, 394, 394, 378, 378, 362, 362, 346, 346, 394, 394, 298, 298, 314, 314, 330, 330, 346, 346, 362, 362, 378, 378, 362, 362, 410, 410, 394, 394, 426, 426, 442, 442, 426, 426, 442, 442, 474, 474, 458, 458, 474, 474, 482];
 const gumb = document.getElementById('gumb');
@@ -14,29 +16,48 @@ let targetX = x[1];
 let targetY = y[1];
 let speed = 1; //1,2,4,8
 let currentIndex = 1;
-function risi() {
-  gumb.disabled = true;
-  gumb2.disabled = true;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 
 
   function draw() {
+    gumb.disabled = true;
+    gumb2.disabled = true;
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.lineWidth = 3;
+
     ctx.beginPath();
     ctx.strokeStyle = 'red';
     ctx.moveTo(currentX, currentY);
 
-
+    
     if (currentX < targetX) {
+      if(currentX + speed >= targetX){
+      currentX=targetX
+      }else{
       currentX += speed;
+      }
     } else if (currentX > targetX) {
-      currentX -= speed;
+      if(currentX - speed <= targetX){
+        currentX=targetX;
+      }else{
+        currentX -= speed;
+      }
     }
 
     if (currentY < targetY) {
-      currentY += speed;
+      if(currentY + speed >= targetY){
+        currentY = targetY;
+      }else{
+        currentY += speed;
+      }
+      
     } else if (currentY > targetY) {
-      currentY -= speed;
+      if(currentY - speed <= targetY){
+        currentY = targetY;
+      }else{
+        currentY -= speed;
+      }
+      
     }
 
     ctx.lineTo(currentX, currentY);
@@ -57,12 +78,11 @@ function risi() {
         currentY = y[0];
         targetX = x[1];
         targetY = y[1];
+        je=false;
       }
     }
   }
 
-  const interval = setInterval(draw, 10);
-}
 
 
 function risibrisi() {
@@ -72,7 +92,7 @@ function risibrisi() {
   let i = 0;
   let trailLength = 60;  // dolžina repa prej kot začnemo za njim brisati
   const trail = [];  // shramba za vse koordinate za brisanje
-
+  
   async function brisi() {
     i++;
 
@@ -143,6 +163,25 @@ function delay(ms) {
 
 var slider = document.getElementById("slider");
 function updateSpeed(){
-  console.log(slider.value);
-  speed =document.querySelector('#slider').value;
+  if(je){
+  clearInterval(interval);
+  var sliderIndex = Math.ceil(slider.value);
+  var hitrost;
+  if(sliderIndex==1)
+    hitrost=1;
+  if(sliderIndex==2)
+    hitrost=2;
+  if(sliderIndex==3)
+    hitrost=4;
+  if(sliderIndex==4)
+    hitrost=8;
+  speed = hitrost;
+  console.log(hitrost);
+  interval = setInterval(draw, 10);
+  }
+}
+function start(){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  je=true;
+  updateSpeed();
 }
